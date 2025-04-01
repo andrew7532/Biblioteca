36a5a5f5228be3a3883937e30d63c1ec099a9a39
andrew7532 <andrewmouradossantos@gmail.com>
2025-03-31 22:19:03 -0300

    Add files via upload

---
+++ Pages/LivrariaLogica/Modelos.cs
@@ -0,0 +1,18 @@
+﻿
+
+public class Livro
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Autor { get; set; }
+    public string Editora { get; set; }
+    public int Ano { get; set; }
+    public string Genero { get; set; }
+    public string ISBN { get; set; }
+    public int Paginas { get; set; }
+    public string Idioma { get; set; }
+    public string Descricao { get; set; }
+    public string Capa { get; set; }
+    public double Preco { get; set; }
+    public int Estoque { get; set; }
+    public bool Ativo { get; set; }
+}
